725de7e95531109453c66f5bbcdaa1df4a21ff82
Brunozo2042 <bruno_henriquemo@hotmail.com>
2023-09-26 08:58:12 -0300

    mudnça de temas funcionando

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 
 changeTheme(1)
 
-criaBotoes(1)
-
 // Função para mudar o tema da página
 function changeTheme(theme) {
     switch (theme) {
@@ -10,24 +8,40 @@ function changeTheme(theme) {
             theme1()
             break;
         case 2:
-
+            theme2()
             break;
         case 3:
-
+            theme3()
             break;
     }
 }
 
 function theme1() {
-    $("body").addClass("body-theme1")
-    $("#teclado").addClass("teclado-theme1")
-    $("#display").addClass("display-theme1")
+    $("body").removeClass().addClass("body-theme1")
+    $("#teclado").removeClass().addClass("teclado-theme1")
+    $("#display").removeClass().addClass("display-theme1")
+    criaBotoes(1)
+}
+
+function theme2() {
+    $("body").removeClass().addClass("body-theme2")
+    $("#teclado").removeClass().addClass("teclado-theme2")
+    $("#display").removeClass().addClass("display-theme2")
+    criaBotoes(2)
+}
+
+function theme3() {
+    $("body").removeClass().addClass("body-theme3")
+    $("#teclado").removeClass().addClass("teclado-theme3")
+    $("#display").removeClass().addClass("display-theme3")
+    criaBotoes(3)
 }
 
 //Função para gerar botões do teclad númerico automaticamente
 function criaBotoes(theme) {
     var teclado = $("#teclado")
 
+    //Limpa o teclaod
     teclado.empty();
 
     var botoes = [["7", "btn1-theme" + theme], ["8", "btn1-theme" + theme], ["9", "btn1-theme" + theme], ["DEL", "btn2-theme" + theme], ["4", "btn1-theme" + theme],
@@ -37,8 +51,11 @@ function criaBotoes(theme) {
 
     for (let i = 0; i < botoes.length; i++) {
         const element = document.createElement("button");
+
+        // Adiciona o conteúdo no botão
         element.textContent = botoes[i][0];
 
+        // Adiciona a classe do botão
         element.classList.add(botoes[i][1])
         element.classList.add("btn")
 
@@ -46,14 +63,18 @@ function criaBotoes(theme) {
             element.classList.add("double")
         }
 
+        // Propriedade para aparecer conteúdo no display ao clicar
         element.setAttribute("onclick", "calcClick(this.textContent)")
 
+        // Insere o elemento no teclado numérico
         teclado.append(element);
     }
 }
 
 function calcClick(val) {
     if (typeof (val) == "Number") {
+
+    } else {
 
     }
 }
